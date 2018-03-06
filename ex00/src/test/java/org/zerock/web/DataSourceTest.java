d40ee446e269e3277bb91c133923b5e99a309146
cho <comeco0724@naver.com>
2018-03-06 11:37:39 +0900

    DataSource설정(MySQL과 연결을 담당) * 에러 발생:log4jdbc 사용하기 위한 DriverManagerDataSource 클래스의 dataSource를 빈으로 생성하지 못함. - 추가사항:log4jdbc.log4j2.properties, logback.xml 파일 생성, pom.xml에 dependency 추가

---
+++ ex00/src/test/java/org/zerock/web/DataSourceTest.java
@@ -0,0 +1,27 @@
+package org.zerock.web;
+
+import java.sql.*;
+
+import javax.inject.*;
+import javax.sql.*;
+
+import org.junit.*;
+import org.junit.runner.*;
+import org.springframework.test.context.*;
+import org.springframework.test.context.junit4.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"file:src/main/webapp/WEB-INF/spring/**/root-context.xml"})
+public class DataSourceTest {
+	@Inject
+	private DataSource ds;
+	
+	@Test
+	public void testConnection() throws Exception {
+		try(Connection con = ds.getConnection()) {
+			System.out.println(con);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
