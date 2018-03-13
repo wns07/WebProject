25c7aa800ba941989fd6c40fd941ce6362600768
cho <comeco0724@naver.com>
2018-03-13 14:12:40 +0900

    Controller 구현(~p200)

---
+++ ex01/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,36 @@
+package org.zerock.controller;
+
+import javax.inject.*;
+
+import org.slf4j.*;
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.domain.*;
+import org.zerock.service.*;
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Inject
+	private BoardService service;
+	
+	@RequestMapping(value="/register", method=RequestMethod.GET)
+	public void registerGET(BoardVO board, Model model) throws Exception {
+		logger.info("register get.............");
+	}
+	
+	@RequestMapping(value="/register", method=RequestMethod.POST)
+	public String registerPOST(BoardVO board, Model model) throws Exception {
+		logger.info("register post.............");
+		logger.info(board.toString());
+		
+		service.regist(board);
+		
+		model.addAttribute("result", "success");
+		
+		return "/board/success";
+	}
+}
